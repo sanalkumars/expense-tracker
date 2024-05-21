7b257406da26339055bf3c841bb8d49a9d6ab8f6
sanalkumars <sanalskumar126@gmail.com>
2024-05-21 10:49:34 +0530

    database setup completed and mongodb connected successfully

---
+++ backend/db/connectDB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected successfully🚀🚀🚀🚀🚀");
+        
+    } catch (error) {
+        console.log("Failde to connect to the DB",error);
+        process.exit(1);
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ import cors from 'cors';
 import { ApolloServer } from '@apollo/server';
 import  mergedResolvers  from './resolvers/index.js'
 import  mergedTypeDef from './typeDefs/index.js';
+import dotenv from 'dotenv'
+import { connectDB } from './db/connectDB.js'
 
+dotenv.config();
 const app = express();
 
 const httpServer = http.createServer(app);
@@ -26,12 +29,14 @@ app.use(
   express.json(),
 
   expressMiddleware(server, {
-    context: async ({ req }) => ({ token: req.headers.token }),
+    context: async ({ req }) => ({ req}),
   }),
 );
    
   
    
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+await connectDB();
 
 console.log(`🚀 Server ready at http://localhost:4000/`);
