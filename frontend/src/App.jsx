9f3f8d914c6e769ca43fcc22e8abdce9b4c69916
sanalkumars <sanalskumar126@gmail.com>
2024-05-23 13:05:22 +0530

    routes are all set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import NotFoundPage from "./pages/NotFoundPage";
+import TransactionPage from "./pages/TransactionPage";
+import SignUpPage from "./pages/SignUpPage";
+import LogInPage from "./pages/LogInPage";
 
-import './App.css'
 
-function App() {
-  
 
-  return (
+function App(){
+
+
+  return(
     <>
-     <h2 className='text-red-500'>hello world</h2>
+    
+    <Routes>
+      <Route path='/' element={ <HomePage />} /> 
+
+      <Route path='/signup' element={ <SignUpPage />} />
+
+      <Route path='/login' element={ <LogInPage />} />
+
+      <Route path='/transaction/:id' element={ <TransactionPage />} />
+
+      <Route path='*' element={ <NotFoundPage />} />
+
+    </Routes>
     </>
   )
 }
-
-export default App
+export default App;
